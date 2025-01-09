f8c3c66d844bb491d53552afd7330af195ae59d0
utsha <utshasaha1234@gmail.com>
2025-01-09 13:45:15 +0600

    create payment api and delete menu item after the payment

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middle ware
@@ -31,6 +32,7 @@ async function run() {
     const menuCollections = client.db("bistroDb").collection("menu");
     const reviewCollections = client.db("bistroDb").collection("reviews");
     const cartCollections = client.db("bistroDb").collection("carts");
+    const paymentCollections = client.db("bistroDb").collection("payments");
 
     // jwt related apis
     app.post("/jwt", (req, res) => {
@@ -192,6 +194,34 @@ async function run() {
       const result = await cartCollections.deleteOne(query);
       res.send(result);
     });
+
+    // payment intent
+    app.post("/create-payment-intent", async(req, res) => {
+      const {price} = req.body;
+      const amount = parseInt(price * 100);
+      console.log(amount, 'amount inside the intent');
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    });
+
+    app.post("/payments", async(req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollections.insertOne(payment);
+
+      console.log('payment info', payment);
+      const query = {_id: {
+        $in: payment.cartIds.map(id => new ObjectId(id))
+      }};
+      const deleteResult = await cartCollections.deleteMany(query);
+      res.send({paymentResult, deleteResult});
+    })
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
